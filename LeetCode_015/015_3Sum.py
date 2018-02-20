e6bdf681bdb799be7d6924160f1d5dcf48ae8e31
daodao1991 <471229015@qq.com>
2018-02-20 11:15:27 +0800

    3Sum_py

---
+++ LeetCode_015/015_3Sum.py
@@ -0,0 +1,33 @@
+# _*_ coding:utf-8 _*_
+
+import os
+
+def threeSum(nums):
+    res = []
+    nums.sort()
+    
+    for i in range(len(nums)):
+        if(nums[i]>0): break
+        if(i>0 and nums[i]==nums[i-1]): continue
+        
+        value = -nums[i]
+        m = i+1
+        n = len(nums)-1
+        while(m < n):
+            if(nums[m] + nums[n] == value):
+                res.append([nums[i],nums[m],nums[n]])
+                m+=1
+                n-=1
+            elif(nums[m] + nums[n] < value):
+                m+=1
+            else:
+                n-=1
+
+    return res
+
+#a = input("请输入一个整数列表：")
+a = [-1, 0, 1, 2, -1, -4]
+result = threeSum(a)
+print(result)
+
+os.system('pause')
