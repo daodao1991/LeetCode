9ea6c748f666347fba470fa9bc873afc599fb9d2
daodao1991 <471229015@qq.com>
2018-02-27 23:32:19 +0800

    023_Merge k Sorted Lists_1

---
+++ LeetCode_023/023_Merge k Sorted Lists_1.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+import os
+
+class ListNode(self, x):
+    self.val = x
+    self.next = None
+
+    
+class Solution():
+    def mergeKLists(self, lists):
+        self.Node = []
+        head = cur = ListNode(0)
+        
+        for l in lists:
+            while(l):
+                self.Node.append(l.val)
+                l = l.next
+        a = sorted(self.Node)
+        for x in a:
+            cur.next = ListNode(x)
+            cur = cur.next
+
+        return head.next
