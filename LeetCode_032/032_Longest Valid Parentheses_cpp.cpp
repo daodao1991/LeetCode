77ec8d98a97d7690b8aec948cfc34d199ea0bc64
daodao1991 <471229015@qq.com>
2018-06-05 09:52:50 +0800

    032_Longest Valid Parentheses_cpp_1

---
+++ LeetCode_032/032_Longest Valid Parentheses_cpp.cpp
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include<iostream>
+#include<string>
+#include<stack>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int longestValidParentheses(string s) {
+		int res = 0, start = 0;
+		stack<int> m;
+		for (int i = 0; i < s.size(); ++i)
+		{
+			if (s[i] == '(')	m.push(i);
+			else if (s[i] == ')')
+			{
+				if (m.empty())	start = i + 1;
+				else
+				{
+					m.pop();
+					res = m.empty() ? max(res, i - start + 1) : max(res, i - m.top());
+				}
+			}
+		}
+
+		return res;
+
+	}
+};
