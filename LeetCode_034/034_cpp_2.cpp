47f337844ec3d7159a270e797fb38387466db3a3
daodao1991 <471229015@qq.com>
2018-08-31 19:54:09 +0800

    对lower_bound()的解释

--- LeetCode_034/034_cpp_2.cpp
+++ LeetCode_034/034_cpp_2.cpp
@@ -15,7 +15,9 @@ public:
 		else
 			return{ -1, -1 };
 	}
-
+	//二分查找的前提是序列已经是有序排列的！！！
+	//C++参考手册中有lower_bound()这个函数，这里相当于自己写了一个lower_bound(),写得很好！！
+	//返回的是一个非递减序列中第一个大于等于值target的位置
 	int lower_bound(vector<int>& nums, int target) {
 		int l = 0, r = nums.size() - 1;
 		while (l <= r) {
