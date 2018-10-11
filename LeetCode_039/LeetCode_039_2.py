befcb4538f4a97fc580f4dc26ff6c1108ca3b3bb
daodao1991 <471229015@qq.com>
2018-10-11 22:39:59 +0800

    LeetCode_039_2.py

---
+++ LeetCode_039/LeetCode_039_2.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+#-*- coding:utf-8 -*-
+
+'''
+    采用深度优先搜寻(DFS)的思想,而DFS的实现要采用递归的方式，
+    python默认的递归深度很有限,大概是900多的样子，当递归深度
+    超过这个值的时候，就会引发这样的一个异常。
+    解决的方式是手工设置递归调用深度，方式为
+'''    
+
+import sys
+sys.setrecursionlimit(1000000)
+
+
+class Solution:
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]]
+        :type target: int
+        :rtype: List[List[int]]]]
+        """
+        res = []
+        temp = []
+        candidates.sort()
+        self.combinationSumDFS(candidates, target, 0, temp, res)
+        return res
+
+
+    def combinationSumDFS(self, candidates, target, start, temp, res):
+        if target < 0:
+            return
+        elif target == 0:
+            res.append(temp)
+        else:
+            for i in range(start, len(candidates)):
+                self.combinationSumDFS(candidates, target-candidates[i], i,
+                        temp+[candidates[i]], res)
+
+
+
+obj = Solution()
+nums = [2, 3, 6, 7]
+res = obj.combinationSum(nums, 7)
+print(res)
+
