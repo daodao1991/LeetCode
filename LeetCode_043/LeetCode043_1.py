29468536a661efa5978a3aadd74fca994bc499db
daodao1991 <471229015@qq.com>
2019-03-03 22:53:26 +0800

    LeetCode043_1.py

---
+++ LeetCode_043/LeetCode043_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+class Solution:
+    def multiply(self, num1, num2):
+        res = 0
+        for i, a in enumerate(num1[::-1]):
+            # 翻转是为了方便计算对应数字的次方数
+            tmp1 = int(a)*(10**i)
+            for j, b in enumerate(num2[::-1]):
+                tmp2 = int(b)*(10**j)
+                res += tmp1 * tmp2
+        return str(res)
